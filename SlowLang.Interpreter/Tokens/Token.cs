3f63d881e9e4ffce9a1ff819198634160afd0fa9
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-23 17:04:17 +0200

    Added the Token class

---
+++ SlowLang.Interpreter/Tokens/Token.cs
@@ -0,0 +1,13 @@
+﻿namespace SlowLang.Interpreter.Tokens;
+
+public class Token
+{
+    public string RawContent;
+    public TokenType Type;
+
+    public Token(string rawContent, TokenType type)
+    {
+        RawContent = rawContent;
+        Type = type;
+    }
+}
