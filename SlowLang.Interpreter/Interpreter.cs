1ce67a277def560176c67ba7b756f2611ec38675
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-24 16:36:14 +0200

    Added a logger to the Interpreter class

--- SlowLang.Interpreter/Interpreter.cs
+++ SlowLang.Interpreter/Interpreter.cs
@@ -1,9 +1,13 @@
-﻿namespace SlowLang.Interpreter;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace SlowLang.Interpreter;
 
 public static class Interpreter
 {
+    private static readonly ILogger Logger = new DebugLoggerProvider().CreateLogger("SusLang.Interpreter");
     public static void RunScript(string code)
     {
-        
+        Lexer.Lex(code);
     }
 }
