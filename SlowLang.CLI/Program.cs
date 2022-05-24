cec5afa6a6197a7177f67edb35150c520ba8c049
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-24 16:35:14 +0200

    Fixed the CLI program

--- SlowLang.CLI/Program.cs
+++ SlowLang.CLI/Program.cs
@@ -1,9 +1,10 @@
-﻿namespace SlowLang.CLI;
+﻿
+namespace SlowLang.CLI;
 
 static class Program
 {
     public static void Main(string[] args)
     {
-        SlowLang.Interpreter.Interpreter.RunScript(args[0]);
+        SlowLang.Interpreter.Interpreter.RunScript(File.ReadAllText(args[0]));
     }
 }
