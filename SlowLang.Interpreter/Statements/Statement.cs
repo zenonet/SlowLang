c93196d4d14c5b8a63ea1909a28ef42d3ae94c24
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-25 16:02:59 +0200

    Created a abstract class Statement

---
+++ SlowLang.Interpreter/Statements/Statement.cs
@@ -0,0 +1,15 @@
+﻿namespace SlowLang.Interpreter.Statements;
+
+/// <summary>
+/// Represents a single statement in a SlowLang script
+/// </summary>
+public abstract class Statement
+{
+    public virtual void Execute()
+    {
+    }
+
+    public virtual void Parse()
+    {
+    }
+}
