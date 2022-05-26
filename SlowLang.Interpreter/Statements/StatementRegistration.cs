9f120378ef72927eb3566633f406b03039671550
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-26 13:11:00 +0200

    Created the StatementRegistration class

---
+++ SlowLang.Interpreter/Statements/StatementRegistration.cs
@@ -0,0 +1,22 @@
+﻿using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Statements;
+
+public struct StatementRegistration
+{
+    public readonly Type Statement;
+    public readonly TokenType[] Match;
+
+    internal StatementRegistration(Type statement, TokenType[] match)
+    {
+        Match = match;
+        Statement = statement;
+    }
+    
+    public static StatementRegistration Create<T>(params TokenType[] match)
+    {
+        return new StatementRegistration(typeof(T), match);
+    }
+    
+    
+}
