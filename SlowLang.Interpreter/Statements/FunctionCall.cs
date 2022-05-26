dfb2ad76f4039f76aaa437ad5fa66783bb46cf07
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-26 13:11:31 +0200

    Created the FunctionCall class

---
+++ SlowLang.Interpreter/Statements/FunctionCall.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Statements;
+
+/// <summary>
+/// Represents a call to a function in a SlowLang script
+/// </summary>
+public class FunctionCall : Statement
+{
+    public static void OnInitialize()
+    {
+        Logger.LogInformation("Now initializing FunctionCall");
+        Statement.Register(StatementRegistration.Create<FunctionCall>(
+            TokenType.Keyword,
+            TokenType.OpeningBrace
+        ));
+    }
+    
+    protected override void OnParse(ref TokenList list)
+    {
+        
+    }
+}
