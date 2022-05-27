6e5ce5fd7d884beb335cde0150b53943cc63da1f
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-27 10:31:25 +0200

    Added a method to Find all inheritors of a specific class

--- SlowLang.Interpreter/ParsingUtility.cs
+++ SlowLang.Interpreter/ParsingUtility.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SlowLang.Interpreter.Statements;
 using SlowLang.Interpreter.Tokens;
 
 namespace SlowLang.Interpreter;
@@ -53,4 +54,20 @@ public static class ParsingUtility
     /// <param name="tokens">The collection of tokens</param>
     /// <returns>The TokenList</returns>
     public static TokenList AsTokenList(this IEnumerable<Token> tokens) => new (tokens);
+
+
+    /// <summary>
+    /// Finds all classes in the current process which derive from a type
+    /// </summary>
+    /// <param name="t">The type, all outputted types should derive from</param>
+    /// <returns>An array of all Types deriving from</returns>
+    public static Type[] GetAllInheritors(Type t)
+    {
+        //Thanks to 'Yahoo Serious' for his answer here: https://stackoverflow.com/questions/857705/get-all-derived-types-of-a-type
+        return (from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+            from assemblyType in domainAssembly.GetTypes()
+            where t.IsAssignableFrom(assemblyType) && t != assemblyType
+            select assemblyType
+            ).ToArray();
+    }
 }
