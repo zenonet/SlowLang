02c4f3ff412190627dfe734b4b81a4e712a90c36
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-27 16:54:24 +0200

    Created the SlowVoid class to represent the return type of a return-less function

---
+++ SlowLang.Interpreter/Values/SlowVoid.cs
@@ -0,0 +1,9 @@
+﻿namespace SlowLang.Interpreter.Values;
+
+/// <summary>
+/// Used as the return type of void functions
+/// </summary>
+public class SlowVoid : Value
+{
+    public static readonly SlowVoid I = new ();
+}
