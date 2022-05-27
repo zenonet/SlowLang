65c35872fc43a20903727d57ffd50823ab553fd5
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-27 16:57:26 +0200

    Created SlowString to hold a string in a SlowLang script

---
+++ SlowLang.Interpreter/Values/SlowString.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Values;
+
+public class SlowString : Value
+{
+    public string Value;
+
+    public SlowString(string value)
+    {
+        Value = value;
+    }
+
+    public static bool TryParse(ref TokenList tokenList, [MaybeNullWhen(false)]out Value val)
+    {
+        if (tokenList.Peek().Type is TokenType.String)
+        {
+            val = new SlowString(
+                tokenList.Pop()     //Get the String token
+                    .RawContent     //Get its raw content
+                    .TrimStart('"') //Remove the " in front
+                    .TrimEnd('"')); //Remove the " at the end
+            return true;
+        }
+
+        val = null;
+        return false;
+    }
+}
