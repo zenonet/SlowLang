dc123bf82fa40987c6556f073204bb163666a1cb
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-27 11:13:54 +0200

    Created the SlowInt class which derives from Value

---
+++ SlowLang.Interpreter/Values/SlowInt.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Values;
+
+public class SlowInt : Value
+{
+    public int Value;
+
+    public SlowInt(int value)
+    {
+        Value = value;
+    }
+    
+    public static bool TryParse(ref TokenList tokenList, [MaybeNullWhen(false)]out Value val)
+    {
+        if (tokenList.Peek().Type is TokenType.Int)
+        {
+            val = new SlowInt(int.Parse(tokenList.Pop().RawContent));
+            return true;
+        }
+
+        val = null;
+        return false;
+    }
+}
