2d21cad26b2d9e431e95dcb97ca52f19bb062999
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-30 17:49:29 +0200

    Made the FunctionCall work

--- SlowLang.Interpreter/Statements/FunctionCall.cs
+++ SlowLang.Interpreter/Statements/FunctionCall.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using SlowLang.Interpreter.Tokens;
+using SlowLang.Interpreter.Values;
 
 namespace SlowLang.Interpreter.Statements;
 
@@ -8,6 +9,17 @@ namespace SlowLang.Interpreter.Statements;
 /// </summary>
 public class FunctionCall : Statement
 {
+    public FunctionDefinition? Reference;
+
+    public Value[] Parameters = null!;
+
+    public FunctionCall(FunctionDefinition? reference, Value[] parameters)
+    {
+        Reference = reference;
+        Parameters = parameters;
+    }
+    public FunctionCall(){}
+
     public static void OnInitialize()
     {
         Logger.LogInformation("Now initializing FunctionCall");
@@ -17,8 +29,35 @@ public class FunctionCall : Statement
         ));
     }
     
+    protected override bool CutTokensManually() => true;
+
     protected override void OnParse(ref TokenList list)
     {
+        //Find the FunctionDefinition this FunctionCall belongs to
+        Reference = FunctionDefinition.GetFunctionDefinition(list.Pop().RawContent);
         
+        //Cut the opening bracket
+        list.Pop();
+        
+        //Find everything between the braces
+        TokenList betweenBraces = ParsingUtility.FindBetweenBraces(list, TokenType.OpeningBrace, TokenType.ClosingBrace, Logger);
+
+        List<Value> parameters = new();
+        foreach (TokenList tokenList in betweenBraces.Split(TokenType.Comma))
+        {
+            parameters.Add(Value.Parse(tokenList)!);
+        }
+
+        this.Parameters = parameters.ToArray();
+        
+        //Remove the rest of the Function call from the TokenList + 1 (The closing bracket) 
+        list.List.RemoveRange(0, betweenBraces.List.Count + 1);
+
+        if (list.Peek() != null && list.Peek().Type is TokenType.Semicolon)
+            list.Pop();
+
     }
+
+    public override Value Execute() => 
+        Reference?.OnInvoke.Invoke(Parameters)!;
 }
