0ec3c5acf1f51aa1d1b9e9e41d7d9d960f9c3f56
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-30 20:24:03 +0200

    Completed the getter statement

--- SlowLang.Interpreter/Statements/Variables/Getter.cs
+++ SlowLang.Interpreter/Statements/Variables/Getter.cs
@@ -1,18 +1,46 @@
 ﻿using Microsoft.Extensions.Logging;
 using SlowLang.Interpreter.Tokens;
+using SlowLang.Interpreter.Values;
 
 namespace SlowLang.Interpreter.Statements.Variables;
 
 public class Getter : Statement
 {
-    
-    
+
+    public string VariableName;
     public static void OnInitialize()
     {
         Logger.LogInformation("Now initializing Getter");
+        
         Statement.Register(StatementRegistration.Create<Getter>(
             TokenType.Keyword,
-            TokenType.Equals
+            TokenType.Semicolon
         ));
+        
+        Statement.Register(StatementRegistration.Create<Getter>(
+            TokenType.Keyword,
+            TokenType.Comma
+        ));
+        
+        Statement.Register(StatementRegistration.Create<Getter>(
+            TokenType.Keyword,
+            TokenType.ClosingBrace
+        ));
+    }
+
+    protected override bool CutTokensManually() => true;
+
+    protected override void OnParse(ref TokenList list)
+    {
+        VariableName = list.Pop().RawContent;
+    }
+
+    public override Value Execute()
+    {
+        if(!Value.Variables.ContainsKey(VariableName))
+            Interpreter.LogError("There is no variable called " + VariableName);
+
+        //Interpreter.LogError makes the process exit so this will only run if the variable exists
+        return Value.Variables[VariableName];
     }
 }
