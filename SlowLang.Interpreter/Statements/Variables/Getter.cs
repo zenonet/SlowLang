d3720871959a3223cb8122890992240aabbbf116
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-30 17:51:37 +0200

    Created the classes Getter and Setter

---
+++ SlowLang.Interpreter/Statements/Variables/Getter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Statements.Variables;
+
+public class Getter : Statement
+{
+    
+    
+    public static void OnInitialize()
+    {
+        Logger.LogInformation("Now initializing Getter");
+        Statement.Register(StatementRegistration.Create<Getter>(
+            TokenType.Keyword,
+            TokenType.Equals
+        ));
+    }
+}
---
+++ SlowLang.Interpreter/Statements/Variables/Setter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Statements.Variables;
+
+public class Setter : Statement
+{
+    public static void OnInitialize()
+    {
+        Logger.LogInformation("Now initializing Setter");
+        Statement.Register(StatementRegistration.Create<Setter>(
+            TokenType.Keyword,
+            TokenType.Equals
+        ));
+    }
+
+
+    protected override bool CutTokensManually() => true;
+
+
+    protected override void OnParse(ref TokenList list)
+    {
+        //Get the variable name
+        string varName = list.Pop().RawContent;
+        //Remove the equals
+        list.Pop();
+
+        Statement.ParseMultiple(list);
+
+    }
+}
