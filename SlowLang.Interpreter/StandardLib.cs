dd9f0492b75eb38145a79359a20856e32cd3a9ac
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-30 17:48:29 +0200

    Added the StandardLib class

---
+++ SlowLang.Interpreter/StandardLib.cs
@@ -0,0 +1,20 @@
+﻿using SlowLang.Interpreter.Statements;
+using SlowLang.Interpreter.Values;
+using static SlowLang.Interpreter.Statements.FunctionDefinition;
+
+namespace SlowLang.Interpreter;
+
+public static class StandardLib
+{
+    public static void Import()
+    {
+        DefinedFunctions.Add(new FunctionDefinition(
+            "print",
+            (parameters) =>
+            {
+                SlowLang.Interpreter.Interpreter.OutputStream?.WriteLine(((SlowString)parameters[0]).Value);
+                return SlowVoid.I;
+            }
+        ));
+    }
+}
