0d8dafc90353324b14fdb9df0b03a7d0c6c0f698
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-30 17:49:11 +0200

    Created the FunctionDefinition class

---
+++ SlowLang.Interpreter/Statements/FunctionDefinition.cs
@@ -0,0 +1,30 @@
+﻿using SlowLang.Interpreter.Values;
+
+namespace SlowLang.Interpreter.Statements;
+
+/// <summary>
+/// Represents the definition of a function in a SlowLang script
+/// </summary>
+public class FunctionDefinition
+{
+    /// <summary>
+    /// Contains all functions that can be called in a SlowLang script
+    /// </summary>
+    public static readonly List<FunctionDefinition> DefinedFunctions = new();
+
+
+    public string Identifier;
+    public Type[] Parameters;
+
+    public readonly Func<Value[], Value> OnInvoke;
+    
+    public FunctionDefinition(string identifier, Func<Value[], Value> onInvoke, params Type[] parameters)
+    {
+        Identifier = identifier;
+        OnInvoke = onInvoke;
+        Parameters = parameters;
+    }
+
+    public static FunctionDefinition GetFunctionDefinition(string name) =>
+        DefinedFunctions.First(fun => fun.Identifier == name);
+}
