9d5556cef869271d4ccd2075b01be4fe85449b96
zenonet <78077158+zenonet@users.noreply.github.com>
2022-05-31 15:17:40 +0200

    Added the CodeBlock class

---
+++ SlowLang.Interpreter/Statements/CodeBlock.cs
@@ -0,0 +1,25 @@
+﻿using SlowLang.Interpreter.Values;
+
+namespace SlowLang.Interpreter.Statements;
+
+/// <summary>
+/// Represents a block of code
+/// </summary>
+public class CodeBlock : Statement
+{
+    /// <summary>
+    /// A list of all statements this CodeBlock contains
+    /// </summary>
+    private readonly Statement[] statements;
+
+    public CodeBlock(Statement[] statements)
+    {
+        this.statements = statements;
+    }
+
+    public override Value Execute()
+    {
+        statements.Execute();
+        return SlowVoid.I;
+    }
+}
