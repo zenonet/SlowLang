5429e8583a772c7c4242b9a402726943873c4432
zenonet <78077158+zenonet@users.noreply.github.com>
2022-06-01 16:31:38 +0200

    Created the CustomParser delegate to replace Func<TokenList, bool>

--- SlowLang.Interpreter/Statements/StatementRegistration.cs
+++ SlowLang.Interpreter/Statements/StatementRegistration.cs
@@ -7,9 +7,10 @@ public struct StatementRegistration
     public readonly Type Statement;
     public readonly TokenType[] Match;
 
-    public Func<TokenList, bool>? CustomParser = null;
+    public CustomParser? CustomParser = null;
 
-    internal StatementRegistration(Type statement, TokenType[] match, Func<TokenList, bool>? customParser = null)
+
+    internal StatementRegistration(Type statement, TokenType[] match, CustomParser? customParser = null)
     {
         Match = match;
         Statement = statement;
@@ -21,10 +22,12 @@ public struct StatementRegistration
         return new StatementRegistration(typeof(T), match);
     }
     
-    public static StatementRegistration CreateWithCustomParser<T>(Func<TokenList, bool>? customParser, params TokenType[] match) where T : Statement
+    public static StatementRegistration CreateWithCustomParser<T>(CustomParser customParser, params TokenType[] match) where T : Statement
     {
         return new StatementRegistration(typeof(T), match, customParser);
     }
     
     
 }
+
+public delegate bool CustomParser(TokenList tokenList);
