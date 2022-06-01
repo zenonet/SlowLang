9a2ffe0652776b9bbaa7d2f927941264b11e6d0d
zenonet <78077158+zenonet@users.noreply.github.com>
2022-06-01 16:19:20 +0200

    Added the option to create a StatementRegistration with a custom parser

--- SlowLang.Interpreter/Statements/StatementRegistration.cs
+++ SlowLang.Interpreter/Statements/StatementRegistration.cs
@@ -7,15 +7,23 @@ public struct StatementRegistration
     public readonly Type Statement;
     public readonly TokenType[] Match;
 
-    internal StatementRegistration(Type statement, TokenType[] match)
+    public Func<TokenList, bool>? CustomParser = null;
+
+    internal StatementRegistration(Type statement, TokenType[] match, Func<TokenList, bool>? customParser = null)
     {
         Match = match;
         Statement = statement;
+        CustomParser = customParser;
     }
     
     public static StatementRegistration Create<T>(params TokenType[] match) where T : Statement
     {
         return new StatementRegistration(typeof(T), match);
+    }
+    
+    public static StatementRegistration CreateWithCustomParser<T>(Func<TokenList, bool>? customParser, params TokenType[] match) where T : Statement
+    {
+        return new StatementRegistration(typeof(T), match, customParser);
     }
     
     
