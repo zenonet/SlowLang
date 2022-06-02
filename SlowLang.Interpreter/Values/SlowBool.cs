ba7366fcb1eae3897f6ed75b8ca528119088c683
zenonet <78077158+zenonet@users.noreply.github.com>
2022-06-02 15:05:36 +0200

    Added the SlowBool type

---
+++ SlowLang.Interpreter/Values/SlowBool.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Values;
+
+public class SlowBool : Value
+{
+    public bool Value;
+
+    public SlowBool(bool value)
+    {
+        Value = value;
+    }
+
+    public static bool TryParse(ref TokenList tokenList, [MaybeNullWhen(false)] out Value val)
+    {
+        if (tokenList.Peek().Type is TokenType.Bool)
+        {
+            val = new SlowBool(bool.Parse(tokenList.Pop().RawContent));
+            return true;
+        }
+
+        val = null;
+        return false;
+    }
+}
