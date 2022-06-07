67d3220b2e50af6523cfe6d82b31d993c13ad596
zenonet <78077158+zenonet@users.noreply.github.com>
2022-06-07 15:50:01 +0200

    Created a builder for StatementRegistrations

---
+++ SlowLang.Interpreter/Statements/StatementRegistrations/StatementRegistration.cs
@@ -0,0 +1,70 @@
+﻿using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Statements.StatementRegistrations;
+
+public readonly struct StatementRegistration
+{
+    public readonly Type Statement;
+    public readonly TokenType[]? Match;
+    public readonly Type? StatementMatch;
+
+    public readonly CustomParser? CustomParser = null;
+
+    internal StatementRegistration(Type statement, TokenType[]? match, CustomParser? customParser = null, Type? statementMatch = null)
+    {
+        Match = match;
+        Statement = statement;
+        CustomParser = customParser;
+        StatementMatch = statementMatch;
+    }
+    
+    /// <summary>
+    /// Creates a Statement Registration from a simple TokenType[] array
+    /// </summary>
+    /// <param name="match">The sequence of Tokens that needs to match</param>
+    /// <typeparam name="T">The statement subclass that should get created when the token sequence gets matched</typeparam>
+    /// <returns>A new StatementRegistration with the settings </returns>
+    public static StatementRegistration Create<T>(params TokenType[] match) where T : Statement
+    {
+        return new StatementRegistration(typeof(T), match);
+    }
+    
+    
+    public static StatementRegistration Create<T>(CustomParser customParser, params TokenType[] match) where T : Statement
+    {
+        return new StatementRegistration(typeof(T), match, customParser);
+    }
+
+    public static StatementRegistration Create<T, TStatementMatch>(CustomParser customParser, params TokenType[] match) where T : Statement where TStatementMatch : Statement
+    {
+        return new StatementRegistration(typeof(T), match, customParser, typeof(TStatementMatch));
+    }
+    
+
+    public void Register()
+    {
+        Statements.Statement.Register(this);
+    }
+
+    /// <summary>
+    /// Creates a new StatementRegistrationBuilder and sets the StatementReference
+    /// </summary>
+    /// <typeparam name="T">The Statement to add add the reference for</typeparam>
+    /// <returns>The preconfigured builder</returns>
+    public static StatementRegistrationBuilder Builder<T>() where T : Statement
+    {
+        return new StatementRegistrationBuilder().AddStatementReference<T>();
+    }
+    
+    public static StatementRegistrationBuilder Builder()
+    {
+        return new StatementRegistrationBuilder();
+    }
+
+    public override string ToString() => Statement.Name + "-Registration";
+}
+
+/// <summary>
+/// Used to define a custom Statement parser
+/// </summary>
+public delegate bool CustomParser(TokenList tokenList);
---
+++ SlowLang.Interpreter/Statements/StatementRegistrations/StatementRegistrationBuilder.cs
@@ -0,0 +1,65 @@
+﻿using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Statements.StatementRegistrations;
+
+public class StatementRegistrationBuilder
+{
+    public static implicit operator StatementRegistration(StatementRegistrationBuilder input) => input.Build();
+
+    private CustomParser? customParser;
+    private TokenType[]? match;
+    private Type? leadingStatement;
+    private Type? statement;
+
+    /// <summary>
+    /// Adds a custom parser to the StatementRegistration
+    /// </summary>
+    /// <param name="parser">A CustomParser delegate that should contain the custom parser</param>
+    public StatementRegistrationBuilder AddCustomParser(CustomParser parser)
+    {
+        customParser = parser;
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a sequence of TokenTypes which need to get matched before the Statement gets instantiated
+    /// </summary>
+    /// <param name="match">The sequence of TokenTypes</param>
+    public StatementRegistrationBuilder AddMatchSequence(params TokenType[] match)
+    {
+        this.match = match;
+        return this;
+    }
+
+    /// <summary>
+    /// Makes the StatementRegistration require a specific Statement in front of this StatementRegistration to gets triggered
+    /// </summary>
+    /// <typeparam name="T">The Statement that needs to get parsed before this one</typeparam>
+    public StatementRegistrationBuilder AddLeadingStatement<T>() where T : Statement
+    {
+        leadingStatement = typeof(T);
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a reference to a Statement that should get instantiated when all the requirements of this StatementRegistration are fulfilled 
+    /// </summary>
+    /// <typeparam name="T">The statement to instantiate</typeparam>
+    public StatementRegistrationBuilder AddStatementReference<T>() where  T : Statement
+    {
+        statement = typeof(T);
+        return this;
+    }
+
+    /// <summary>
+    /// Builds this StatementRegistrationBuilder to a StatementRegistration
+    /// </summary>
+    /// <returns>The built StatementRegistration</returns>
+    public StatementRegistration Build()
+    {
+        if (statement is null)
+            throw new(
+                $"A StatementRegistration needs to have a StatementReference selected. Select one using StatementRegistrationBuilder.{nameof(AddStatementReference)}");
+        return new StatementRegistration(statement, match, customParser, leadingStatement);
+    }
+}
