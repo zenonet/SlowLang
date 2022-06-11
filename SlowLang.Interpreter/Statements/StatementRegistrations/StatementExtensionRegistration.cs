cecd5cfbfc6b458070016b78fff74af992be52b6
zenonet <78077158+zenonet@users.noreply.github.com>
2022-06-11 20:08:20 +0200

    Created the StatementExtensionRegistration struct

---
+++ SlowLang.Interpreter/Statements/StatementRegistrations/StatementExtensionRegistration.cs
@@ -0,0 +1,27 @@
+﻿using SlowLang.Interpreter.Tokens;
+
+namespace SlowLang.Interpreter.Statements.StatementRegistrations;
+
+public struct StatementExtensionRegistration
+{
+    public readonly Statement BaseStatement;
+    public readonly Statement ExtensionStatement;
+    public readonly CustomParser? CustomParser;
+    public readonly TokenType[] Match;
+
+    public StatementExtensionRegistration(Statement extensionStatement, Statement baseStatement, CustomParser customParser, params TokenType[] match)
+    {
+        Match = match;
+        CustomParser = customParser;
+        ExtensionStatement = extensionStatement;
+        BaseStatement = baseStatement;
+    }
+    
+    public StatementExtensionRegistration(Statement extensionStatement, Statement baseStatement, params TokenType[] match)
+    {
+        Match = match;
+        CustomParser = null;
+        ExtensionStatement = extensionStatement;
+        BaseStatement = baseStatement;
+    }
+}
